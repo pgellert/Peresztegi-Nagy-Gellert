145075c523c8a43e3687f30f64453c3e1c2759a9
Gellért Peresztegi- Nagy <gellpn22@gmail.com>
2016-01-23 10:19:30 +0100

    Base changes

---
+++ app/src/main/java/com/gellert/pereszteginagygellert/MainActivity.java
@@ -0,0 +1,38 @@
+package com.gellert.pereszteginagygellert;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.contact:
+                Intent intent = new Intent(MainActivity.this,ContactActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+}
